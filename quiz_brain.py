c672217229fb4b24eb0aca58626a4acd7eb39851
Tareq Judeh <tareq.judeh@outlook.com>
2021-01-04 11:59:36 +0200

    Updated data.py using Trivia DB API, instead of hard coded data. Usage of html library helped in unescaping unwanted HTML characters.

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,5 @@
+import html
+
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
@@ -11,8 +13,9 @@ class QuizBrain:
         """Prompt and Ask the user a question:"""
         current_question = self.question_list[self.question_number]
         self.question_number += 1
+        q_text = html.unescape(current_question.text)
         user_answer = input(
-            f"Q.{self.question_number}: {current_question.text} (True/False): ")
+            f"Q.{self.question_number}: {q_text} (True/False): ")
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
